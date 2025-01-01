83e90c4ce7d94242b8a2f9fe45ed75efd2280bfa
Zoon20X <47308184+Zoon20X@users.noreply.github.com>
2025-01-01 12:09:19 +1000

    updated pom to 1.6

--- src/main/java/me/zoon20x/network/File/ConfigSection.java
+++ src/main/java/me/zoon20x/network/File/ConfigSection.java
@@ -7,25 +7,31 @@ import java.util.Map;
 public class ConfigSection {
     private Map<String, Object> configData = new HashMap<>();
 
-    public ConfigSection(Map<String, Object> configData){
+    public ConfigSection(Map<String, Object> configData) {
         this.configData = configData;
     }
-    public int getInteger(String value){
+
+    public int getInteger(String value) {
         return (int) configData.get(value);
 
     }
-    public String getString(String value){
+
+    public String getString(String value) {
         return (String) configData.get(value);
     }
-    public boolean getBoolean(String value){
+
+    public boolean getBoolean(String value) {
         return (boolean) configData.get(value);
     }
-    public double getDouble(String value){
+
+    public double getDouble(String value) {
         return (double) configData.get(value);
     }
-    public void set(String value, Object data){
+
+    public void set(String value, Object data) {
         configData.put(value, data);
     }
+
 
     public Map<String, Object> getConfigSectionData() {
         return configData;
